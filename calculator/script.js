e19d108f734816f95b9660edf426ca7b6f204df9
Christina Kashevar <christi_k@mail.ru>
2020-09-23 23:49:07 +0300

    feat: add addition, subtraction, multiplication, division, square root, plus/minus

---
+++ calculator/script.js
@@ -0,0 +1,184 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement;
+      this.currentOperandTextElement = currentOperandTextElement;
+      this.readyToReset = false;
+      this.clear();
+    }
+  
+    clear() {
+      this.currentOperand = '';
+      this.previousOperand = '';
+      this.operation = undefined;
+    }
+  
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+  
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return;
+      this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+  
+    chooseOperation(operation) {
+    if (this.currentOperandTextElement.innerText === "Error" ) {
+        this.currentOperand = 0;
+    }
+      if (this.currentOperand === '') return;
+      if (this.previousOperand !== '') {
+        this.compute();
+      }
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = '';
+    }
+  
+    compute() {
+      let computation;
+      const prev = parseFloat(this.previousOperand);
+      const current = parseFloat(this.currentOperand);
+      if (isNaN(prev) || isNaN(current)) return;
+      switch (this.operation) {
+        case '+':
+          computation = prev + current;
+          break
+        case '-':
+          computation = prev - current;
+          break
+        case '*':
+          computation = prev * current;
+          break
+        case '÷':
+          computation = prev / current;
+          break
+        case '**':
+          computation = prev ** current;
+          break
+        default:
+          return;
+      }
+      this.readyToReset = true;
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = '';
+    }
+
+    sqrt() {
+        if(parseFloat(this.currentOperand) < 0) {
+            this.currentOperandTextElement.innerText = "Error"
+        } else {
+            let computationSqrt = Math.sqrt(parseFloat(this.currentOperand));
+            this.currentOperand = computationSqrt;
+            this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        }
+    }
+
+    exponent() {
+        let computationSqrt = Math.sqrt(parseFloat(this.currentOperand));
+        this.currentOperand = computationSqrt;
+        this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+    }
+
+    plusMinus () {
+        this.currentOperand = this.currentOperand.toString();
+        if (this.currentOperand.startsWith('-')) {
+          this.currentOperand = this.currentOperand.slice(1)
+        } else {
+          this.currentOperand = `-${this.currentOperand}`
+        }
+    }
+  
+    getDisplayNumber(number) {
+      const stringNumber = number.toString()
+      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const decimalDigits = stringNumber.split('.')[1]
+      let integerDisplay
+      if (isNaN(integerDigits)) {
+        integerDisplay = ''
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      }
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+      } else {
+        return integerDisplay
+      }
+    }
+  
+    updateDisplay() {
+      this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.innerText = ''
+      }
+    }
+}
+  
+  
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const sqrtButton = document.querySelector('[data-sqrt]');
+const exponentButton = document.querySelector('[data-exponentiation]');
+const plusMinusButton = document.querySelector('[data-plus-minus]')
+  
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+  
+        if(calculator.previousOperand === "" &&
+        calculator.currentOperand !== "" && 
+        calculator.readyToReset) {
+            calculator.currentOperand = "";
+            calculator.readyToReset = false;
+        }
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay();
+    })
+})
+  
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText);
+      calculator.updateDisplay();
+    })
+})
+  
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+  
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+  
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+sqrtButton.addEventListener('click', button => {
+    calculator.sqrt();
+})
+
+exponentButton.addEventListener('click', button => {
+    calculator.exponent();
+})
+
+plusMinusButton.addEventListener('click', button => {
+    calculator.plusMinus();
+    calculator.updateDisplay();
+})
