1a1f0eb973b529eab09661de2cb7e60296028c68
Christina Kashevar <christi_k@mail.ru>
2020-10-15 16:25:35 +0300

    feat: change algorithm - result only after button equal

--- calculator/script.js
+++ calculator/script.js
@@ -4,83 +4,181 @@ class Calculator {
       this.currentOperandTextElement = currentOperandTextElement;
       this.readyToReset = false;
       this.clear();
+      this.calculationArray =[]
     }
   
     clear() {
       this.currentOperand = '';
       this.previousOperand = '';
       this.operation = undefined;
+      this.calculationArray = [];
     }
   
     delete() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+      if (this.currentOperandTextElement.innerText === "Error") {
+        this.currentOperand = '';
+      } else {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+      }
     }
   
     appendNumber(number) {
       if (number === '.' && this.currentOperand.includes('.')) return;
+      if (this.currentOperandTextElement.innerText === "Error") {
+        this.currentOperand = '';
+      }
       this.currentOperand = this.currentOperand.toString() + number.toString();
     }
   
     chooseOperation(operation) {
-    if (this.currentOperandTextElement.innerText === "Error" ) {
+      if (this.currentOperandTextElement.innerText === "Error" ) {
         this.currentOperand = 0;
-    }
-      if (this.currentOperand === '') return;
-      if (this.previousOperand !== '') {
-        this.compute();
       }
-      this.operation = operation;
-      this.previousOperand = this.currentOperand;
-      this.currentOperand = '';
+      if (this.currentOperand === '') return;
+      if (this.currentOperand === '√') { // если последняя операция √, то можно нажимать кнопку с операцией
+          this.currentOperand = '';
+          this.operation = operation;
+          this.calculationArray.push(this.operation);
+          this.previousOperand +=' '+ this.operation;
+          return;
+      }
+      if (this.operation === '÷') {
+        this.checkPossibilityOfCompute();
+      }
+      if (this.currentOperand != "Error") {
+        this.operation = operation;
+        this.calculationArray.push(this.currentOperand, this.operation)
+        this.previousOperand +=' '+ this.currentOperand +' '+ this.operation;
+        this.currentOperand = '';
+      }
+    }
+
+    sqrt (operation) {
+      if (this.currentOperand < 0 ) {
+          this.currentOperand = "Error";
+          return
+     } else {
+        this.operation = operation;
+        this.calculationArray.push( this.operation, this.currentOperand)
+        this.previousOperand +=' '+ this.operation +' '+ this.currentOperand;
+        this.currentOperand = this.operation;
+     }
+    }
+
+    checkPossibilityOfCompute() {
+        if (this.operation === '√' && this.currentOperand < 0 ||
+            this.operation === '÷' && +this.currentOperand === 0) {
+                this.currentOperand = "Error";
+        }
     }
   
     compute() {
-      let computation;
-      const prev = parseFloat(this.previousOperand);
-      const current = parseFloat(this.currentOperand);
-      if (isNaN(prev) || isNaN(current)) return;
-      switch (this.operation) {
-        case '+':
-          computation = prev + current;
-          break
-        case '-':
-          computation = prev - current;
-          break
-        case '*':
-          computation = prev * current;
-          break
-        case '÷':
-          computation = prev / current;
-          break
-        case '**':
-          computation = prev ** current;
-          break
-        default:
-          return;
+      this.checkPossibilityOfCompute();
+      let result;
+      let maxDig;
+      if (this.currentOperand === "Error") {
+        this.updateDisplay();
+        return
       }
-      computation = Math.round(computation * 1000000000000000) / 1000000000000000;
+      if (this.previousOperand === '' || this.currentOperand === '') return;
+      this.calculationArray.push(this.currentOperand)
+      let computation = 0;
+
+      // проверяем окончание строки previousOperand, если последнее значение не цифра - удаляем
+      let lastElement = this.calculationArray[this.calculationArray.length-1];
+      if ( isNaN(parseFloat(lastElement)) ) {
+        this.calculationArray.pop()
+      }
+
+      let counter = 0;
+      let counter2 = 0;
+      for (let i =0; i< this.calculationArray.length; i++) {
+        if ( this.calculationArray[i] === '*'|| this.calculationArray[i] === '÷' ||
+             this.calculationArray[i] === '√' || this.calculationArray[i] === '**'){
+              ++counter
+          }
+      }
+
+      for (let i =0; i< this.calculationArray.length; i++) {
+        if ( this.calculationArray[i] === '-'|| this.calculationArray[i] === '+'){
+              ++counter2
+          }
+      }
+      
+      for (let i =0; i < counter; i++ ) {
+          console.log(this.calculationArray)
+        this.calculationArray.map( (item,index) => {
+            switch (item) {
+                case '*':
+                    result = this.makeFractionCorrect(parseFloat(this.calculationArray[index-1]), parseFloat(this.calculationArray[index+1]))
+                    computation =( parseFloat(result[0]) * parseFloat(result[1]) ) / ((10**result[2]) * (10**result[3]))
+                    computation += ''
+                    this.calculationArray.splice(index-1, 3, computation);
+                    break
+                case '÷':
+                    result = this.makeFractionCorrect(parseFloat(this.calculationArray[index-1]), parseFloat(this.calculationArray[index+1]))
+                    computation =( parseFloat(result[0]) / parseFloat(result[1]) ) / ((10**result[2]) * (10**result[3]))
+                    computation += ''
+                    this.calculationArray.splice(index-1, 3, computation)
+                    break
+                case '√':
+                    computation = Math.sqrt(parseFloat(this.calculationArray[index+1]));
+                    computation += '';
+                    this.calculationArray.splice(index, 2, computation)
+                    break
+                case '**':
+                    result = this.makeFractionCorrect(parseFloat(this.calculationArray[index-1]), parseFloat(this.calculationArray[index+1]));
+                    computation = ( parseFloat(result[0]) ** parseFloat(result[1]) ) / ((10**result[2]) ** result[1]);
+                    computation += ''
+                    this.calculationArray.splice(index-1, 3, computation)
+                    break
+                default:
+                    return;
+            }
+        })
+      }
+
+      for (let i =0; i < counter2; i++ ) {
+        console.log(this.calculationArray)
+        this.calculationArray.map( (item,index) => {
+            switch (item) {
+                case '+':
+                    result = this.makeFractionCorrect(parseFloat(this.calculationArray[index-1]), parseFloat(this.calculationArray[index+1]))
+                    maxDig = Math.max(result[2], result[3]);
+                    computation = ( parseFloat(this.calculationArray[index-1])* (10** maxDig) + parseFloat(this.calculationArray[index+1])* (10** maxDig)) / (10** maxDig)
+                    computation += ''
+                    this.calculationArray.splice(index-1, 3, computation)
+                    break
+                case '-':
+                    result = this.makeFractionCorrect(parseFloat(this.calculationArray[index-1]), parseFloat(this.calculationArray[index+1]))
+                    maxDig = Math.max(result[2], result[3])
+                    computation = ( parseFloat(this.calculationArray[index-1])* (10** maxDig) - parseFloat(this.calculationArray[index+1])* (10** maxDig)) / (10** maxDig)
+                    computation += ''
+                    this.calculationArray.splice(index-1, 3, computation)
+                    break
+                default:
+                    return;
+            }
+        })
+      }
+      computation = Math.round(computation * 100000000) / 100000000;
       this.readyToReset = true;
       this.currentOperand = computation;
       this.operation = undefined;
       this.previousOperand = '';
+      this.calculationArray = [];
     }
 
-    sqrt() {
-        if(parseFloat(this.currentOperand) < 0) {
-            this.currentOperandTextElement.innerText = "Error"
-        } else {
-            let computationSqrt = Math.sqrt(parseFloat(this.currentOperand));
-            this.currentOperand = computationSqrt;
-            this.currentOperandTextElement.innerText =
-            this.getDisplayNumber(this.currentOperand)
-        }
-    }
-
-    exponent() {
-        let computationSqrt = Math.sqrt(parseFloat(this.currentOperand));
-        this.currentOperand = computationSqrt;
-        this.currentOperandTextElement.innerText =
-        this.getDisplayNumber(this.currentOperand)
+    makeFractionCorrect (operand1, operand2) {
+      let firstDigitsAfterComa;
+      let secondDigitsAfterComa;
+      firstDigitsAfterComa = (operand1.toString().includes('.')) ? (operand1.toString().split('.').pop().length) : (0) ;
+      operand1 = operand1 * (10**firstDigitsAfterComa);
+      if (operand2 !== undefined) {
+        secondDigitsAfterComa = (operand2.toString().includes('.')) ? (operand2.toString().split('.').pop().length) : (0) ;
+        operand2 = operand2 * (10**secondDigitsAfterComa);
+      }
+      return ([operand1, operand2, firstDigitsAfterComa, secondDigitsAfterComa ])
     }
 
     plusMinus () {
@@ -105,14 +203,17 @@ class Calculator {
     }
   
     updateDisplay() {
-      this.currentOperandTextElement.innerText =
-        this.getDisplayNumber(this.currentOperand)
-      if (this.operation != null) {
-        this.previousOperandTextElement.innerText =
-          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-      } else {
-        this.previousOperandTextElement.innerText = ''
-      }
+        if (this.currentOperand === "Error") {
+            this.currentOperandTextElement.innerText = "Error"
+        } else {
+            this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        }
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = this.previousOperand
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
   
@@ -124,20 +225,18 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const plusMinusButton = document.querySelector('[data-plus-minus]');
 const sqrtButton = document.querySelector('[data-sqrt]');
-const exponentButton = document.querySelector('[data-exponentiation]');
-const plusMinusButton = document.querySelector('[data-plus-minus]')
   
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
   
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-  
         if(calculator.previousOperand === "" &&
-        calculator.currentOperand !== "" && 
-        calculator.readyToReset) {
-            calculator.currentOperand = "";
-            calculator.readyToReset = false;
+           calculator.currentOperand !== "" &&
+           calculator.readyToReset) {
+              calculator.currentOperand = "";
+              calculator.readyToReset = false;
         }
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay();
@@ -166,11 +265,12 @@ deleteButton.addEventListener('click', button => {
     calculator.updateDisplay();
 })
 
-sqrtButton.addEventListener('click', button => {
-    calculator.sqrt();
-})
-
 plusMinusButton.addEventListener('click', button => {
     calculator.plusMinus();
     calculator.updateDisplay();
+})
+
+sqrtButton.addEventListener('click', button => {
+  calculator.sqrt('√');
+  calculator.updateDisplay();
 })
